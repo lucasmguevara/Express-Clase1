05f1052b9fb74602c2b32ab5a6639d91319ca78a
Lucas Guevara <lucasguevaracba@gmail.com>
2022-07-07 21:05:07 -0300

    Se incluyeron vistas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
+const path = require('path');
+
 require('dotenv').config()
 
 const express = require('express');
 const app = express();
+
+app.get('/', function(req, res) {
+    // res.send('<h1>Bienvenido</h1>'); // Devuelve un texto
+    res.sendFile(path.join(__dirname, '/views/Index.html')); // Devuelve un archivo
+})
+
+app.get('/contacto', function(req, res) {
+    // res.send('<h1>Bienvenido a la página de contacto</h1>');
+    res.sendFile(path.join(__dirname, '/views/Contacto.html')); // Devuelve un archivo
+})
 
 app.listen(process.env.PUERTO, function() {
     console.log("Servidor iniciado en puerto: " + process.env.PUERTO + "...")
